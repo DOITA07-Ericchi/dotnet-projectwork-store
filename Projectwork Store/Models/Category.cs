0f103020c66829e945af3f619c5b30c93ae5b602
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-27 11:20:03 +0100

    Modello Category per la tipologia delle auto

---
+++ Projectwork Store/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projectwork_Store.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [StringLength(100, ErrorMessage = "Il campo del nome non può contenere più di 100 caratteri")]
+        public string Name { get; set; }
+
+        //rel. 1-n car
+        public List<Car> Cars { get; set; }
+
+        public Category()
+        {
+
+        }
+    }
+}
