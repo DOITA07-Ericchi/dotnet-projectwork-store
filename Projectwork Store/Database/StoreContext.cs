6091f463106811fbc01eac5183b5d01772069817
GioPa97 <giovannipagliara97@.com>
2023-01-27 15:28:29 +0100

    Creato Database

---
+++ Projectwork Store/Database/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projectwork_Store.Models;
+
+namespace Projectwork_Store.Database
+{
+        public class StoreContext : DbContext
+        {
+            public DbSet<Car> Cars { get; set; }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Sticker> Stickers { get; set; }
+
+
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=ericchi.moe;Database=ItashaStoreDevelop;User Id=SA;Password=DatabaseCaconeDegenerato;TrustServerCertificate=True");
+            }
+        }
+    }
