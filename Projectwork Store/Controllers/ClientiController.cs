89b8b0b9ba8bb1b4c6f0888fc18017c325dfbb7c
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-30 10:46:56 +0100

    aggiunti i controller di clienti e cars

---
+++ Projectwork Store/Controllers/ClientiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projectwork_Store.Controllers
+{
+    public class ClientiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
