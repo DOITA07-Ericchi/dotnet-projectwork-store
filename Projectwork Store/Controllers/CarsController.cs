77d62d25590158cbcbcc2463f494610e6a1eb4c7
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-30 10:55:05 +0100

    Creato metodo get api con la ricerca

--- Projectwork Store/Controllers/CarsController.cs
+++ Projectwork Store/Controllers/CarsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projectwork_Store.Database;
+using Projectwork_Store.Models;
 
 namespace Projectwork_Store.Controllers
 {
@@ -7,6 +10,31 @@ namespace Projectwork_Store.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get(string? search)
+        {
+            using (StoreContext db = new StoreContext())
+            {
+                List<Car> cars = new List<Car>();
 
+                if (search is null || search == "")
+                {
+                    cars = db.Cars.Include(car => car.Category).Include(pizza => pizza.Sticker).ToList<Car>();
+                }
+                else
+                {
+                    search = search.ToLower();
+
+                    cars = db.Cars.Where(car => car.Name.ToLower().Contains(search))
+                        .Include(car => car.Category)
+                        .Include(pizza => pizza.Sticker).ToList<Car>();
+                }
+
+                return Ok(cars);
+
+            }
+
+
+        }
     }
 }
