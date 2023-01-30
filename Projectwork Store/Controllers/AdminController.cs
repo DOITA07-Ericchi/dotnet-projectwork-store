4557cfe3924f8736c853cd9f8c37fd2ffd5fa8a9
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-30 09:31:44 +0100

    Creato controller di Admin

---
+++ Projectwork Store/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projectwork_Store.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult ConfirmDelete()
+        {
+            return View();
+        }
+        public IActionResult Storage()
+        {
+            return View();
+        }
+    }
+}
