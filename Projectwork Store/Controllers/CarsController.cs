8749c5f2ae22ced2216d05e01e5b8ed80773dbf1
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-01-30 11:00:33 +0100

    creato metodo per mostarare il dettaglio

--- Projectwork Store/Controllers/CarsController.cs
+++ Projectwork Store/Controllers/CarsController.cs
@@ -36,5 +36,43 @@ namespace Projectwork_Store.Controllers
 
 
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+
+            using (StoreContext db = new StoreContext())
+            {
+                Car cars = db.Cars.Where(pizza => pizza.Id == id).Include(car => car.Category).Include(pizza => pizza.Sticker).FirstOrDefault();
+
+                if (cars is null)
+                {
+                    return NotFound("L'auto non è stata trovata!");
+                }
+
+                return Ok(cars);
+            }
+        }
+        public IActionResult Dettaglio(int id)
+        {
+
+
+            using (StoreContext db = new StoreContext())
+
+            {
+                Car cars = db.Cars.Where(pizza => pizza.Id == id).Include(car => car.Category).Include(pizza => pizza.Sticker).FirstOrDefault();
+                if (cars != null)
+                {
+                    return Ok(cars);
+                }
+
+                return NotFound("La pizza con l'id cercato non esiste!");
+
+            }
+
+
+
+        }
+
     }
 }
