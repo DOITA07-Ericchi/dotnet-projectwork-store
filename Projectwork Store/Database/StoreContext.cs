32b1f80594e95c94ea7dcb0d9959af972b370307
GioPa97 <giovannipagliara97@.com>
2023-01-31 11:58:14 +0100

    Add Purchase

--- Projectwork Store/Database/StoreContext.cs
+++ Projectwork Store/Database/StoreContext.cs
@@ -3,17 +3,18 @@ using Projectwork_Store.Models;
 
 namespace Projectwork_Store.Database
 {
-        public class StoreContext : DbContext
+    public class StoreContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Sticker> Stickers { get; set; }
+        public DbSet<UserPurchase> UserPurchases { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            public DbSet<Car> Cars { get; set; }
-            public DbSet<Category> Categories { get; set; }
-            public DbSet<Sticker> Stickers { get; set; }
-
-
-
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                optionsBuilder.UseSqlServer("Data Source=ericchi.moe;Database=ItashaStoreDevelop;User Id=SA;Password=DatabaseCaconeDegenerato;TrustServerCertificate=True");
-            }
+            optionsBuilder.UseSqlServer("Data Source=ericchi.moe;Database=ItashaStoreDevelop;User Id=SA;Password=DatabaseCaconeDegenerato;TrustServerCertificate=True");
         }
     }
+}
---
+++ Projectwork Store/Models/UserPurchase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Projectwork_Store.Models
+{
+    public class UserPurchase
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime PurchaseData { get; set; }
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50, ErrorMessage = "Il testo fornito non deve superare i 50 caratteri")]
+        public string Name { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50, ErrorMessage = "Il testo fornito non deve superare i 50 caratteri")]
+        public string Surname { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50, ErrorMessage = "La mail fornita non deve superare i 50 caratteri")]
+        public string Email { get; set; }
+        [Column(TypeName = "varchar(20)")]
+        [StringLength(20, ErrorMessage = "Il numero fornito non deve superare i 20 caratteri")]
+        public string Phone { get; set; }
+
+        //rel 1-n car
+        [JsonIgnore]
+        public List<Car> Cars { get; set; }
+
+        public UserPurchase()
+        {
+
+        }
+
+    }
+}
