5766cf4ded6a35900bc98482b487391d6e22dbb1
DrLiveasy <youssef.saleh.hans@gmail.com>
2023-02-09 13:50:55 +0100

    Modello per la view index

---
+++ Projectwork Store/Models/ViewClassifica.cs
@@ -0,0 +1,8 @@
+﻿namespace Projectwork_Store.Models
+{
+    public class ViewClassifica
+    {
+        public List<Car>? Cars { get; set; }
+        public List<UserPurchase>? Purchases { get; set; }
+    }
+}
